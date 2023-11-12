7127a70db231446fd38100a78081b9024beb4ab6
Apicqq <apic@yandex.ru>
2023-11-12 08:25:07 +0300

    Add recipe viewset to router

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, TagViewSet
+from api.views import IngredientViewSet, TagViewSet, RecipeViewSet
 
 v1_router = DefaultRouter()
 
 v1_router.register('ingredients', IngredientViewSet, 'ingredients')
 v1_router.register('tags', TagViewSet, 'tags')
+v1_router.register('recipes', RecipeViewSet, 'recipes')
 
 urlpatterns = [
     path('', include('djoser.urls')),
