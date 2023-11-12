a96cc0e82b567bebc256b6054b8ca5c3f03adf9c
Apicqq <apic@yandex.ru>
2023-11-12 08:17:45 +0300

    Add some configuration to admin panel, not done yet

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Recipe, Ingredient, RecipeIngredient
+
+admin.site.empty_value_display = 'Не задано'
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color', 'slug')
+    search_fields = ('name', 'color', 'slug')
+    list_filter = ('name', 'color', 'slug')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'get_favorites')
+    search_fields = ('name', 'author')
+    list_filter = ('name', 'author', 'tags')
+    inlines = (RecipeIngredientInline,)
+
+    def get_favorites(self, obj):
+        return obj.favorites.count()
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('name',)
