00fab32ae6d0233b60ccce16199c9ac7089d6d5d
Apicqq <apic@yandex.ru>
2023-11-12 08:18:58 +0300

    Add tags uploader to DB, add tag json template

--- backend/foodgram/api/management/commands/upload_tags.py
+++ backend/foodgram/api/management/commands/upload_tags.py
@@ -2,11 +2,11 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Tag
 
 
 class Command(BaseCommand):
-    help = 'Import ingredients from json file'
+    help = 'Import tags from json file'
 
     def add_arguments(self, parser):
         parser.add_argument('file', type=str)
@@ -15,10 +15,11 @@ class Command(BaseCommand):
         try:
             with open(options.get('file'), encoding='utf-8') as file:
                 data = json.load(file)
-            for ingredient in data:
-                Ingredient.objects.get_or_create(
-                    name=ingredient['name'],
-                    measurement_unit=ingredient['measurement_unit'],
+            for tag in data:
+                Tag.objects.get_or_create(
+                    name=tag.get('name'),
+                    color=tag.get('color'),
+                    slug=tag.get('slug')
                 )
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR(f'File "{file}" not found'))
