84e4dc504192a0bb330424e2dd03271e3ab1f0e9
Apicqq <apic@yandex.ru>
2023-11-14 20:12:18 +0300

    Fix urls, make them more logical

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,22 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, TagViewSet, RecipeViewSet, \
-    UserSubscriptionsViewSet
+from api.views import (IngredientViewSet, TagViewSet, RecipeViewSet,
+                       UserViewSet)
 
 v1_router = DefaultRouter()
 
 v1_router.register('ingredients', IngredientViewSet, 'ingredients')
 v1_router.register('tags', TagViewSet, 'tags')
 v1_router.register('recipes', RecipeViewSet, 'recipes')
-# v1_router.register('users', SubscriptionsViewSet, 'users')
-# v1_router.register(r'users/(?P<user_id>\d+)/subscribe', SubscribeRelations, 'adddeletesubscription')
+v1_router.register('users', UserViewSet, 'users')
 
 urlpatterns = [
-    path('users/subscriptions/',
-         UserSubscriptionsViewSet.as_view({'get': 'list'})),
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
     path('', include(v1_router.urls)),
-
+    path('auth/', include('djoser.urls.authtoken'))
 ]
