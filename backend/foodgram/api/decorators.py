35ad8101803112648f0d5ea6358c59405e747a88
Apicqq <apic@yandex.ru>
2023-11-14 20:06:47 +0300

    Add decorator to wrap boolean checks in serializers

---
+++ backend/foodgram/api/decorators.py
@@ -0,0 +1,15 @@
+from functools import wraps
+
+
+def get_related_queryset(related_name, related_object_arg):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, obj):
+            request = self.context.get('request')
+            return (request and request.user.is_authenticated
+                    and getattr(request.user, related_name).filter(
+                        **{related_object_arg: obj}).exists())
+
+        return wrapper
+
+    return decorator
