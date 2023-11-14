37c2b0cdf8190ebdc922d69cd0e0fdb4b90c0fd5
Apicqq <apic@yandex.ru>
2023-11-14 11:09:07 +0300

    Add RecipeFiter

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,6 +1,6 @@
 from django_filters import rest_framework as filters
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Recipe
 
 
 class IngredientFilter(filters.FilterSet):
@@ -10,3 +10,24 @@ class IngredientFilter(filters.FilterSet):
     class Meta:
         model = Ingredient
         fields = ('name',)
+
+
+class RecipeFilter(filters.FilterSet):
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = filters.BooleanFilter(method='_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='_in_shopping_cart')
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
+
+    def _is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return self.queryset.filter(in_favorites__user=self.request.user)
+        return queryset
+
+    def _in_shopping_cart(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return self.queryset.filter(carts__user=self.request.user)
+        return queryset
