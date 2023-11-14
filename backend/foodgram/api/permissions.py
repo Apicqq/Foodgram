d57874346678017e3870ecfa138a1b25184b8f30
Apicqq <apic@yandex.ru>
2023-11-14 11:10:15 +0300

    Change permission name to match its logic

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAdminAuthorOrReadOnly(BasePermission):
+class IsAuthorOrReadOnly(BasePermission):
     message = 'Действие недоступно для вас.'
 
     def has_permission(self, request, view):
