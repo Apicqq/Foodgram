65cd2a95e91b4471d31683d0c2a76147719359e2
Apicqq <apic@yandex.ru>
2023-11-15 23:11:45 +0300

    Add pre-review cosmetic improvements

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import TokenProxy
 
-from .models import Tag, Recipe, Ingredient, RecipeIngredient
+from core.constants import RecipeConstants
+from .models import Tag, Recipe, Ingredient, RecipeIngredient, ShoppingCart
 
-admin.site.empty_value_display = 'Не задано'
+admin.site.empty_value_display = RecipeConstants.ADMIN_EMPTY_VALUE
+
+User = get_user_model()
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -26,9 +32,36 @@ class RecipeAdmin(admin.ModelAdmin):
     def get_favorites(self, obj):
         return obj.in_favorites.count()
 
+    get_favorites.short_description = RecipeConstants.FAVORITES_DESCRIPTION
+
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'measurement_unit')
     search_fields = ('name',)
     list_filter = ('name',)
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'ingredient', 'amount')
+    search_fields = ('recipe', 'ingredient')
+    list_filter = ('recipe', 'ingredient')
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'email')
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    search_fields = ('user', 'recipe')
+    list_filter = ('user', 'recipe')
+
+
+admin.site.unregister(TokenProxy)
+admin.site.unregister(Group)
