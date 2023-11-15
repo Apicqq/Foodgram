86232fbb7591a682ccd608dfdb3c974d6d0fac75
Apicqq <apic@yandex.ru>
2023-11-15 23:13:08 +0300

    Add pre-review cosmetic improvements

--- backend/foodgram/api/decorators.py
+++ backend/foodgram/api/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 
-
 def get_related_queryset(related_name, related_object_arg):
+    """Декоратор для упаковки повторяющегося кода."""
     def decorator(func):
         @wraps(func)
         def wrapper(self, obj):
@@ -13,3 +13,5 @@ def get_related_queryset(related_name, related_object_arg):
         return wrapper
 
     return decorator
+
+# return request.user.follower.filter(author=obj).exists
