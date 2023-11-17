c4bac10a00f61f76393893d53f33a16018cd4d61
Apicqq <apic@yandex.ru>
2023-11-17 07:04:43 +0300

    Minor update allowed hosts variable

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY',
 
 DEBUG = os.getenv('DEBUG_STATUS') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['*']).split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
