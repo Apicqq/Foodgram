b52f2543f7630b4fa337937510ec8eb247192d71
Apicqq <apic@yandex.ru>
2023-11-19 06:40:10 +0300

    Update management scripts

--- backend/api/management/commands/upload_ingredients.py
+++ backend/api/management/commands/upload_ingredients.py
@@ -1,6 +1,8 @@
 import json
 
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+from tqdm import tqdm
 
 from recipes.models import Ingredient
 
@@ -8,18 +10,19 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
     help = 'Import ingredients from json file'
 
-    def add_arguments(self, parser):
-        parser.add_argument('file', type=str)
-
     def handle(self, *args, **options):
         try:
-            with open(options.get('file'), encoding='utf-8') as file:
+            with open('data/ingredients.json', encoding='utf-8') as file:
                 data = json.load(file)
-            for ingredient in data:
+            for ingredient in tqdm(data, colour='green'):
                 Ingredient.objects.get_or_create(
                     name=ingredient['name'],
                     measurement_unit=ingredient['measurement_unit'],
                 )
+            self.stdout.write(
+                    self.style.SUCCESS('Data was successfully imported'))
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR(f'File "{file}" not found'))
-        self.stdout.write(self.style.SUCCESS('Data was successfully imported'))
+        except IntegrityError:
+            self.stdout.write(self.style.ERROR(f'Ingredients located in {file}'
+                                               f'already exist'))
--- backend/api/management/commands/upload_tags.py
+++ backend/api/management/commands/upload_tags.py
@@ -1,6 +1,8 @@
 import json
 
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
+from tqdm import tqdm
 
 from recipes.models import Tag
 
@@ -8,19 +10,21 @@ from recipes.models import Tag
 class Command(BaseCommand):
     help = 'Import tags from json file'
 
-    def add_arguments(self, parser):
-        parser.add_argument('file', type=str)
-
     def handle(self, *args, **options):
         try:
-            with open(options.get('file'), encoding='utf-8') as file:
+            with open('data/tags.json', encoding='utf-8') as file:
                 data = json.load(file)
-            for tag in data:
+            for tag in tqdm(data, colour='green'):
                 Tag.objects.get_or_create(
                     name=tag.get('name'),
                     color=tag.get('color'),
                     slug=tag.get('slug')
                 )
+            self.stdout.write(
+                self.style.SUCCESS('Data was successfully imported'))
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR(f'File "{file}" not found'))
-        self.stdout.write(self.style.SUCCESS('Data was successfully imported'))
+        except IntegrityError:
+            self.stdout.write(self.style.ERROR(f'Tags located in {file}'
+                                               f'already exist'))
+
