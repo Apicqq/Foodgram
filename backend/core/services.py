499946fa2880ff5ae1e14f2790f55b8237fddb33
Apicqq <apic@yandex.ru>
2023-11-19 22:00:16 +0300

    First review — services update

--- backend/core/services.py
+++ backend/core/services.py
@@ -1,38 +1,24 @@
-from django.db.models import Sum
 from rest_framework import status
-from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
-from recipes.models import Ingredient, RecipeIngredient
+from recipes.models import RecipeIngredient
 
 
 def pass_ingredients(ingredients, recipe):
     recipe_ingredients = [RecipeIngredient(
         recipe=recipe,
-        ingredient=get_object_or_404(Ingredient, pk=ingredient.get('id')),
+        ingredient=ingredient.get('id'),
         amount=ingredient.get('amount')
     ) for ingredient in ingredients]
     RecipeIngredient.objects.bulk_create(recipe_ingredients)
-    return
 
 
-def get_subscription_ingredients(request):
+def prepare_ingredients_list(ingredients):
     shopping_list = []
-    ingredients = (
-        request.user.shoppingcarts.values(
-            'recipe__ingredients__name',
-            'recipe__ingredients__measurement_unit'
-        )
-        .annotate(
-            ingredient_amount=Sum(
-                'recipe__recipeingredients__amount'
-            )
-        )
-    )
     for item in ingredients:
-        name = item.get('recipe__ingredients__name', 'Не указано')
+        name = item.get('ingredient__name', 'Не указано')
         measurement_unit = (
-            item.get('recipe__ingredients__measurement_unit', 'г.'))
+            item.get('ingredient__measurement_unit', 'г.'))
         amount = item.get('ingredient_amount', 0)
         shopping_list.append(
             f'\n{name.capitalize()} - {amount}{measurement_unit}\n'
@@ -40,37 +26,20 @@ def get_subscription_ingredients(request):
     return ' '.join(shopping_list)
 
 
-def _create_related_object(pk,
-                           request,
-                           serializer_class,
-                           subscription_arg=False
-                           ):
-    if subscription_arg:
-        serializer = serializer_class(
-            data={
-                'user': request.user.id,
-                'author': pk},
-            context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-    else:
-        serializer = serializer_class(
-            data={
-                'user': request.user.id,
-                'recipe': pk},
-            context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+def _create_related_object(pk, request, serializer_class):
+    serializer = serializer_class(
+        data={
+            'user': request.user.id,
+            'recipe': pk},
+        context={'request': request})
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-def _delete_related_object(pk, request, model, subscription_arg=False):
-    try:
-        if subscription_arg:
-            model.objects.filter(user=request.user, author=pk).delete()
-        else:
-            model.objects.filter(user=request.user, recipe=pk).delete()
-    except model.ObjectDoesNotExist:
+def _delete_related_object(pk, request, model):
+    if not model.objects.filter(user=request.user, recipe=pk).exists():
         return Response(status=status.HTTP_400_BAD_REQUEST,
-                        data={'error': 'Запрашиваемый объект не найден.'})
+                        data={'message': 'Запрашиваемый объект не найден'})
+    model.objects.filter(user=request.user, recipe=pk).delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
