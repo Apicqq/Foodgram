f9e3cafd9765061c053da3c9419dfd2f510d15ba
Apicqq <apic@yandex.ru>
2023-11-19 21:54:35 +0300

    First review — update antipattern file name

--- backend/core/pdf_reporter.py
+++ backend/core/pdf_reporter.py
@@ -6,10 +6,10 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 
-from core.services import get_subscription_ingredients
+from core.services import prepare_ingredients_list
 
 
-def draw_pdf_report(request):
+def draw_pdf_report(items):
     """Рисуем отчёт о предстоящих покупках в виде PDF-файла."""
     buffer = io.BytesIO()
     p = canvas.Canvas(buffer)
@@ -19,7 +19,7 @@ def draw_pdf_report(request):
         300, 800,
         'Ваш список покупок:')
     text_object = p.beginText(2 * cm, 29.7 * cm - 2 * cm)
-    for line in get_subscription_ingredients(request).splitlines(False):
+    for line in prepare_ingredients_list(items).splitlines(False):
         text_object.textLine(line.rstrip())
     p.drawText(text_object)
     p.showPage()
