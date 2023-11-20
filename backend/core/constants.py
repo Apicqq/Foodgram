73738633f0fd1a48bbe18099363260ab2ba560ae
Apicqq <apic@yandex.ru>
2023-11-20 22:52:21 +0300

    Second review — constants fix

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -11,30 +11,16 @@ class RecipeConstants:
     MAX_STR_LENGTH = 200
     TAG_COLOR_LENGTH = 7
     STR_RETURN_VALUE = 30
-    RECIPE_TEXT_LENGTH = 10000
-    COOKING_TIME_MAX_VALUE_VALIDATOR_VALUE = 50
-    MAXIMUM_AMOUNT_REQUIRED = 300
+    MAX_COOKING_TIME = 43800  # месяц в минутах.
+    MAXIMUM_AMOUNT_ALLOWED = 10000  # 10 кг.
     MIN_VALUE = 1
 
 
 class UserConstants:
     """Константы для приложения пользователей."""
 
-    USER_USERNAME_LENGTH = 150
+    USER_CREDENTIALS_MAX_LENGTH = 150
     USER_EMAIL_LENGTH = 254
-    USER_FIRST_NAME_LENGTH = 150
-    USER_LAST_NAME_LENGTH = 150
     STR_RETURN_VALUE = 30
     RECIPES_AMOUNT = 'Количество рецептов'
     SUBSCRIBERS_AMOUNT = 'Количество подписчиков'
-
-
-class APIConstants:
-    """Константы для прилодения API."""
-
-    API_MIN_VALUE = 1
-    API_MAX_VALUE_INGREDIENTS = 50
-    API_MAX_VALUE_COOKING_TIME = 360
-    API_MIN_VALUE_ERROR_MESSAGE_INGREDIENTS = 'Минимальное значение: 1'
-    API_MAX_VALUE_ERROR_MESSAGE_INGREDIENTS = 'Максимальное значение: 50'
-    API_MAX_VALUE_ERROR_COOKING_TIME_MESSAGE = 'Максимальное значение: 360'
